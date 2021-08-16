029069a56869e51fc0251c1ac7998904444bc3b3
Jane <happyjane210@gmail.com>
2021-08-16 19:44:34 +0900

    2021-08-16

---
+++ modern-JavaScript-tutorial/2021-08-11/01.DOM-3.js
@@ -0,0 +1,14 @@
+// 배경색 1초 바꿨다가 원상복귀
+// document.body.style.background = "green";
+// setTimeout(() => document.body.style.background = "", 1000);
+
+// location: 객체는 현재 url을 읽게해주고 새로운 url로 변경할수 있게 함.
+// alert(location.href);
+// if (confirm("블로그로 이동하시겠습니까?")) {
+//     location.href = "https://blog.naver.com/jjoon0210"
+// };
+
+// 현재 사용중인 브라우저 정보
+alert(navigator.userAgent);
+// 브라우저가 실행중인 운영체제 정보
+alert(navigator.platform);
